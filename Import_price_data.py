0cf6da91da8ddc9756405714d2fd153e5b66787d
Brynne Fitzgerald <brynnefitzgerald@gmail.com>
2021-07-13 15:39:19 -0400

    Using yfinance
    Server is down for yahoo via datareader

--- Import_price_data.py
+++ Import_price_data.py
@@ -1,3 +1,4 @@
+import yfinance as yf
 import talib
 from pandas_datareader import DataReader
 from pandas_datareader import data as web
@@ -9,18 +10,18 @@ import statsmodels.api as smf
 import math
 import pandas.util.testing as tm
 import matplotlib.pyplot as plt
-%matplotlib inline
 plt.style.use('fivethirtyeight')
 
 #Set start/end dates to pull historic price data
-end = dt.datetime.today()
-start = (dt.datetime.now() - dt.timedelta(days=6000)).strftime("%m-%d-%Y")
+end = dt.datetime.today().strftime("%Y-%m-%d")
+start = (dt.datetime.now() - dt.timedelta(days=1000)).strftime("%m-%d-%Y")
 
-ticker = 'UPST'
+#ticker = 'SPY'
+ticker = yf.Ticker("AAPL")
 #Request historic price data from pandas datareader
-df = web.DataReader(f'{ticker}', data_source='yahoo', start=start)
-
+#df = web.DataReader(f'{ticker}', data_source='yahoo', start=start)
+df = ticker.history(period="2y")
 #Create Lags
-df['CloseLag1'] = df['Adj Close'].shift(-1)
-df['CloseLag2'] = df['Adj Close'].shift(-2)
-df['Close'] = df['Adj Close']
+df['CloseLag1'] = df['Close'].shift(-1)
+df['CloseLag2'] = df['Close'].shift(-2)
+#df['Close'] = df['Adj Close']
